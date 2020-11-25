18b65167c4fbbdada7df218abf8e5087c8910cd8
maorrocky <maorrocky@gmail.com>
2020-11-25 10:51:57 +0200

    hi

---
+++ src/com/company/Main.java
@@ -0,0 +1,16 @@
+package com.company;
+
+import java.util.HashMap;
+
+public class Main {
+
+    public static void main(String[] args) {
+        int[] nums = {1, 2, 3, 1, 1, 3};
+        System.out.println("numIdenticalPairs(nums) = " + numIdenticalPairs(nums));
+    }
+
+
+
+}
+
+
